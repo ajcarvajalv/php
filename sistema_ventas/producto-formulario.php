a53800d6e29bbeba5a168f3c0986d29915cf1a82
ajcarvajalv <ajcarvajalv@gmail.com>
2022-09-07 14:05:53 -0500

    Cambio en producto-formulario.php

--- sistema_ventas/producto-formulario.php
+++ sistema_ventas/producto-formulario.php
@@ -17,8 +17,8 @@ $aTiposProductos = $tipoProducto->obtenertodos();
 
         <div class="row">
                 <div class="col-12 mb-3">
-                    <a href="tipoproducto-listado.php" class="btn btn-primary mr-2">Listado</a>
-                    <a href="tipoproducto-formulario.php" class="btn btn-primary mr-2">Nuevo</a>
+                    <a href="producto-listado.php" class="btn btn-primary mr-2">Listado</a>
+                    <a href="producto-formulario.php" class="btn btn-primary mr-2">Nuevo</a>
                     <button type="submit" class="btn btn-success mr-2" id="btnGuardar" name="btnGuardar">Guardar</button>
                     <button type="submit" class="btn btn-danger" id="btnBorrar" name="btnBorrar">Borrar</button>
                 </div>
